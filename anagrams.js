30f6cd62e9f2ff1a1e3f882e654f59dca547fa3b
Tristan Alie <TristanAlie@yahoo.com>
2021-09-02 10:49:20 -0400

    added an event listener for pressing the enter key and made the submit functionality its own function

--- anagrams.js
+++ anagrams.js
@@ -7,11 +7,17 @@ const userInput = document.getElementById('textBox')
 const submit = document.getElementById('submitButton')
 const results = document.getElementById('anagrams')
 
+const submitInput = () => { //makes a function to submit the input so i can put it on the submit button and pressing enter on the keyboard
+    let lowerWord = userInput.value
+    let input = lowerWord.toLowerCase() // all the words are in lowercase
+    findAnagram(input)
+}
+
 const findAnagramInWords = (word) => { //function that finds anagrams from a given word
     let anagramArray = [] //holds the words that are anagrams
 
     words.map((anaWord) => { //goes through all the words and finds the anagrams
-        if(anaWord.split('').sort() === word.split('').sort()) { //taking the string and converting it into an array so it can be sorted to find the anagrams, i thought strings were already arrays, but i guess not
+        if(anaWord.split('').sort().join('') === word.split('').sort().join('')) { //taking the string and converting it into an array so it can be sorted to find the anagrams, i thought strings were already arrays, but i guess not
             anagramArray.push(anaWord);
         }
     })
@@ -25,18 +31,24 @@ const findAnagram = (input) => { //puts the anagrams into the results div
 }
 
 submit.addEventListener('click', () => { //gets the input from the text box and runs the anagram function
-    let lowerWord = userInput.value
-    let input = lowerWord.toLowerCase() // all the words are in lowercase
-    findAnagram(input)
+    submitInput()
+})
+
+userInput.addEventListener('keydown', e => {
+    if (e.keyCode === 13) {
+        submitInput()
+    }
 })
 
 const findMultipleAnagrams = () => { //it's not complete, but it want to get to a point where i can check if it's working
     let successArray = [] //the array of the succeeding anagrams
+
     for (let i = 0; i < words.length; i++) {
         let currentWord = findAnagramInWords(words[i]) //gets an array of all anagrams of the current word
         if(currentWord.length >= 5) { //only add to the success array if it's at least five words
             successArray.push(currentWord.join(', ')) //makes each array one value so it's easier to read
         }
     }
+    
     return successArray//returns the success array, but somehow it acts like an infinite loop before it gets to this point
 }
