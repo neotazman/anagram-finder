8ecb4919b8d2979edbe2b77d17ea280db2ee0d90
Tristan Alie <TristanAlie@yahoo.com>
2021-09-26 15:31:16 -0400

    intermediate challenge works, but takes 17 minutes to complete

--- anagrams.js
+++ anagrams.js
@@ -8,6 +8,7 @@ const userInput = document.getElementById('textBox')
 const submit = document.getElementById('submitButton')
 const results = document.getElementById('anagrams')
 const setsOfFive = document.getElementById('fiveAnagrams')
+let anagramObject = {}
 
 const submitInput = () => { //makes a function to submit the input so i can put it on the submit button and pressing enter on the keyboard
     let lowerWord = userInput.value
@@ -17,7 +18,6 @@ const submitInput = () => { //makes a function to submit the input so i can put 
 
 const findAnagramInWords = (word) => { //function that finds anagrams from a given word !!!!RETURNS AN OBJECT NOW!!!!
     //let anagramArray = [] //holds the words that are anagrams
-    let anagramObject = {}
 
     words.map((anaWord) => { //goes through all the words and finds the anagrams
         if(anaWord.split('').sort().join('') === word.split('').sort().join('')) { //taking the string and converting it into an array so it can be sorted to find the anagrams, i thought strings were already arrays, but i guess not
@@ -28,11 +28,11 @@ const findAnagramInWords = (word) => { //function that finds anagrams from a giv
             }
         }
     })
-    return anagramObject
+    return anagramObject[word.split('').sort().join('')]
 }
 
 const findAnagram = (input) => { //puts the anagrams into the results div
-    let result = findAnagramInWords(input)[input.split(',')[0].split('').sort().join('')].join(', '); //turns the array on the object into a string that can be written on the page
+    let result = findAnagramInWords(input).join(', '); //turns the array on the object into a string that can be written on the page
     results.innerText = '' //resets the results div when a new submission is made
     results.append(result)
 }
@@ -47,14 +47,14 @@ userInput.addEventListener('keydown', e => { //so pressing enter in the text box
     }
 })
 
-const findMultipleAnagrams = () => { //it's not complete, but it want to get to a point where i can check if it's working
+const findMultipleAnagrams = () => { //it works, but it takes 17 minutes to complete
     let successArray = [] //the array of the succeeding anagrams
     let successObject = {}
 
     for (let i = 0; i < words.length; i++) {
         if(words[i].length > 3) {
-            let currentWord = findAnagramInWords(words[i]) //gets an array of all anagrams of the current word
-            if(currentWord.length >= 5) { //only add to the success array if it's at least five words
+            let currentWord = findAnagramInWords(words[i])//[words[i].split('').join('')] //gets an array of all anagrams of the current word
+            if(currentWord.length >= 5 && !successArray.includes(currentWord.join(', '))) { //only add to the success array if it's at least five words and it doesn't match another array in the success array
                 successArray.push(currentWord.join(', ')) //makes each array one value so it's easier to read
                 console.log(currentWord)
             }
@@ -62,7 +62,7 @@ const findMultipleAnagrams = () => { //it's not complete, but it want to get to 
 
     }
     
-    return successArray//returns the success array, but somehow it acts like an infinite loop before it gets to this point
+    return successArray//returns the success array
 }
 
 setsOfFive.addEventListener('click', () => { //what happens when the button for the intermediate challenge is clicked
@@ -71,6 +71,6 @@ setsOfFive.addEventListener('click', () => { //what happens when the button for 
     let result = findMultipleAnagrams() //runs the function, currently running too slowly
     
     container.innerText = result //i don't think i need to stringify it if i'm setting it as inner text, but i might be wrong
-    console.log(container)
+    console.log(container) //just in case the above line doesn't run
     results.append(container)
 })
